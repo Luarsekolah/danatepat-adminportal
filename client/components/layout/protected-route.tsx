277bdfd5586b4ddbe176418773052e4f3546412e
abdul <up2dul@gmail.com>
2025-12-29 17:55:53 +0700

    feat(client/comps/layout): create ProtectedRoute component for authentication handling

---
+++ client/components/layout/protected-route.tsx
@@ -0,0 +1,19 @@
+import { Outlet, useLocation, useNavigate } from "react-router";
+import env from "@/env";
+import { useAuthStore } from "@/stores/auth-store";
+
+export const ProtectedRoute = () => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    clearAuth();
+    if (location.pathname.startsWith("/dashboard")) {
+      window.location.href = env.VITE_APP_BASE_URL;
+    }
+    return null;
+  }
+
+  return <Outlet />;
+};
