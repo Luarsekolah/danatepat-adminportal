8edaba8ff1f92a5e7b507f7cfcec8c035b649686
abdul <up2dul@gmail.com>
2026-01-06 13:57:51 +0700

    feat(App): remove unused Toaster component and update Sonner usage

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,5 @@
 import "./global.css";
 
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -30,8 +29,7 @@ const queryClient = new QueryClient({
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      <Sonner richColors />
       <BrowserRouter>
         <Routes>
           <Route path="dashboard" element={<ProtectedRoute />}>
